a8d91fe0994385abc1fee1d1ec92f57154de23ca
mujiaci <1187725424@qq.com>
2026-07-28 11:40:04 +0800

    restore padding calculation imports after split

--- app/src/main/java/me/rerere/rikkahub/ui/pages/extensions/PromptPaddingValues.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/extensions/PromptPaddingValues.kt
@@ -1,6 +1,8 @@
 package me.rerere.rikkahub.ui.pages.extensions
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.ui.unit.LayoutDirection
 
 internal operator fun PaddingValues.plus(other: PaddingValues): PaddingValues = PaddingValues(
